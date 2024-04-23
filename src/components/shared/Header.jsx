de5fc6bc678af1bc546ae89812ecc283005ca2d9
JuanCTrujillo <150824287+DarkKiller31@users.noreply.github.com>
2024-04-23 00:33:01 -0500

    Implementacion de menu hamburguesa

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -8,8 +8,19 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { MdOutlineWbSunny } from "react-icons/md";
 import { FiMoon } from "react-icons/fi";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 const Header = ({ t, changeLanguage, modoDyL, setModoDyL }) => {
+
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const handleOpen = () => {
+    setOpenMenu(true)
+    if(openMenu){
+      setOpenMenu(false)
+    }
+  }
 
   const [lang, setLang] = useState(false)
 
@@ -28,36 +39,43 @@ const Header = ({ t, changeLanguage, modoDyL, setModoDyL }) => {
 
   return (
     <header className='header__container'>
-      <Link className='header__li'  to='/'>
+      <Link className='header__li Img'  to='/'>
         <img className='header__logo' src="../logo.svg" alt="" />
       </Link>
-      <ul className='header__list'>
-        <li>
-          <Link className='header__li'  to='/'><AiOutlineHome className='header__icon' /> <h3 className='header__link'>{t("BTN1")}</h3></Link>
-        </li>
-        <li>
-          <Link className='header__li' to='/about'><FaRegUser className='header__icon' /> <h3 className='header__link'>{t("BTN2")}</h3></Link>
-        </li>
-        <li>
-          <Link className='header__li' to='/skills'><BsPersonGear className='header__icon' /> <h3 className='header__link'>{t("BTN3")}</h3></Link>
-        </li>
-        <li>
-          <Link className='header__li' to='/projects'><PiToolboxBold className='header__icon' /> <h3 className='header__link'>{t("BTN4")}</h3></Link>
-        </li>
-        <li>
-          <Link className='header__li' to='/contact'><MdOutlinePhonelink className='header__icon' /> <h3 className='header__link'>{t("BTN5")}</h3></Link>
-        </li>
-      </ul>
-      <div className='container__btns'>
-        <div className='header__language'>
-          <div className='iconsDL' onClick={() => changeLanguage("en") & changeCircleEn(true) }><img className={`header__img ${lang ?'img__none' : ''}`} src='../english.png'></img></div>
-          <div className='iconsDL' onClick={() => changeLanguage("es") & changeCircleEs(false) }><img className={`header__img ${lang ?'' : 'img__none'}`} src='../spanish.png'></img></div>
+      <nav className='header__inter'>
+      <div className="nav__toggle" onClick={handleOpen}>
+          <RxHamburgerMenu />
         </div>
-        <div className='header__toggle'>
-          <div className='iconsDL' onClick={toggleLight}><MdOutlineWbSunny className={`iconLight iconTo ${modoDyL ?'img__none' : ''}`} /></div>
-          <div className='iconsDL' onClick={toggleDark}><FiMoon className={`iconDark iconTo ${modoDyL ?'' : 'img__none'}`} /></div>
+        <div className={`content__list ${openMenu ?'list__open' : ''}`}>
+            <ul className='header__list'>
+              <li>
+                <Link className='header__li'  to='/'><AiOutlineHome className='header__icon' /> <h3 onClick={handleOpen} className='header__link'>{t("BTN1")}</h3></Link>
+              </li>
+              <li>
+                <Link className='header__li' to='/about'><FaRegUser className='header__icon' /> <h3 onClick={handleOpen} className='header__link'>{t("BTN2")}</h3></Link>
+              </li>
+              <li>
+                <Link className='header__li' to='/skills'><BsPersonGear className='header__icon' /> <h3 onClick={handleOpen} className='header__link'>{t("BTN3")}</h3></Link>
+              </li>
+              <li>
+                <Link className='header__li' to='/projects'><PiToolboxBold className='header__icon' /> <h3 onClick={handleOpen} className='header__link'>{t("BTN4")}</h3></Link>
+              </li>
+              <li>
+                <Link className='header__li' to='/contact'><MdOutlinePhonelink className='header__icon' /> <h3 onClick={handleOpen} className='header__link'>{t("BTN5")}</h3></Link>
+              </li>
+            </ul>
+            <div className='container__btns'>
+              <div className='header__language'>
+                <div className='iconsDL' onClick={() => changeLanguage("en") & changeCircleEn(true) }><img className={`header__img ${lang ?'img__none' : ''}`} src='../english.png'></img></div>
+                <div className='iconsDL' onClick={() => changeLanguage("es") & changeCircleEs(false) }><img className={`header__img ${lang ?'' : 'img__none'}`} src='../spanish.png'></img></div>
+              </div>
+              <div className='header__toggle'>
+                <div className='iconsDL' onClick={toggleLight}><MdOutlineWbSunny className={`iconLight iconTo ${modoDyL ?'img__none' : ''}`} /></div>
+                <div className='iconsDL' onClick={toggleDark}><FiMoon className={`iconDark iconTo ${modoDyL ?'' : 'img__none'}`} /></div>
+              </div>
+            </div>
         </div>
-      </div>
+      </nav>
     </header>
   )
 }
